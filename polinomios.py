ee293a6c5066b57458d2f2ce5463d58c1191697c
Rafaella Lenzi <lenzirafaella@gmail.com>
2022-07-15 09:15:59 -0300

    Update polinomios.py

--- polinomios.py
+++ polinomios.py
@@ -1,66 +1,62 @@
 class Polinomio:
-
     def __init__(self, coeficientes=[]):
-        self.coeficiente=coeficientes #recebendo os coeficientes como uma lista de inteiros
-
-    def __setitem__(self, grau, coeficiente): #adapta os coeficientes para o grau dado
-        if grau<len(self.coeficiente):
-            self.coeficiente[grau]=coeficiente
-        elif grau>len(self.coeficiente):
-            self.coeficiente+=[0]*(grau-len(self.coeficiente))
-            self.coeficiente.append(coeficiente)
-        else:#se o tamanho da lista de coeficientes for igual ao valor do grau
-            self.coeficiente.append(coeficiente)
-
+        self.coe=coeficientes
+        
+    def __setitem__(self, grau, coeficiente):
+        if grau<len(self.coe):
+            self.coe[grau]=coeficiente
+        else:
+            self.coe+=[0]*(grau-len(self.coe))
+            self.coe.append(coeficiente)
+            
     def __getitem__(self,grau):
-        return self.coeficiente[grau] #a posição corresponde ao grau, posição 3 -> grau 3
-
-    def grau (self):
-        return len(self.coeficiente)-1 #retorna o grau do polinômio (-1 por causa do x^0)
-
-    def __mul__(self,p):
-        r=[]
-        a=[]
-        if len(self.coeficiente)>len(p.coeficiente):
-            p.coeficiente+=[0]*(len(self.coeficiente)-len(p.coeficiente))
-        elif len(self.coeficiente)<len(p.coeficiente):
-            self.coeficiente+=[0]*(len(p.coeficiente)-len(self.coeficiente))#faz com que os polinomios tenham o mesmo tamanho
-        for i in range(len(self.coeficiente)):
-            for j in range(len(p.coeficiente)):
-                r.append(self.coeficiente[i]*p.coeficiente[j])
-                if len(r) == len(self.coeficiente):
-                    a.append(Polinomio(r).avalia(x))
-                    r = []
-        return Polinomio(a)
+        return self.coe[grau]
         
-    def __add__(self,p):
+    def grau(self):
+        return len(self.coe)-1
+        
+    def __add__(self,other):
         r=[]
-        if len(self.coeficiente)>len(p.coeficiente):
-            p.coeficiente+=[0]*(len(self.coeficiente)-len(p.coeficiente))
-        elif len(self.coeficiente)<len(p.coeficiente):
-            self.coeficiente+=[0]*(len(p.coeficiente)-len(self.coeficiente))#faz com que os polinomios tenham o mesmo tamanho
-        for i in range(len(self.coeficiente)):
-            r.append(self.coeficiente[i]+p.coeficiente[i])#soma cada index de um com cada index do outro
+        if len(self.coe)>len(other.coe):
+            other.coe+=[0]*(len(self.coe)-len(other.coe))
+        elif len(self.coe)<len(other.coe):
+            self.coe+=[0]*(len(other.coe)-len(self.coe))
+        for i in range(len(self.coe)):
+            r.append(self.coe[i]+other.coe[i])
         return Polinomio(r)
-
-    def __sub__(self,p):
+    
+    def __sub__(self,other):
         r=[]
-        if len(self.coeficiente)>len(p.coeficiente):
-            p.coeficiente+=[0]*(len(self.coeficiente)-len(p.coeficiente))
-        elif len(self.coeficiente)<len(p.coeficiente):
-            self.coeficiente+=[0]*(len(p.coeficiente)-len(self.coeficiente))#faz com que os polinomios tenham o mesmo tamanho
-        for i in range(len(self.coeficiente)):
-            r.append(self.coeficiente[i]-p.coeficiente[i])#subtrai cada index de um com cada index do outro
+        if len(self.coe)>len(other.coe):
+            other.coe+=[0]*(len(self.coe)-len(other.coe))
+        elif len(self.coe)<len(other.coe):
+            self.coe+=[0]*(len(other.coe)-len(self.coe))
+        for i in range(len(self.coe)):
+            r.append(self.coe[i]-other.coe[i])
         return Polinomio(r)
-
-    def avalia (self,x):
+        
+    def __mul__(self,other):
+        r=[]
+        r+=[0]*(len(self.coe)+len(other.coe))
+        for i in range(len(self.coe)):
+            for j in range(len(other.coe)):
+                r[i+j]+=self.coe[i]*other.coe[j]
+        return Polinomio(r)
+        
+    def avalia(self,x):
         r=0
-        for i in range(len(self.coeficiente)):
-            r+=self.coeficiente[i]*x**i 
+        for i in range(len(self.coe)):
+            r+=self.coe[i]*x**i
         return r
-        
-p=Polinomio(eval(input()))   
-q=Polinomio(eval(input()))
-x=eval(input())
+            
+p = Polinomio ([1,2])
+p[3] = 0.5
+q = Polinomio ([0,-1,2])
 
-print(p.avalia(x),q.avalia(x),(p+q).avalia(x),(p-q).avalia(x),(p*q).avalia(x))
+print(p[0],p[1],p[2])
+
+print(p.avalia(0))
+
+print(p.avalia(3),q.avalia(3),(p+q).avalia(3),(p-q).avalia(3),(p*q).avalia(3))
+
+print(((p+q)*(p-q)).avalia(0.5))
